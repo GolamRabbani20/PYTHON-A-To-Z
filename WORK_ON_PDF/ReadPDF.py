8ed1427bb26cec962d3500959861071160c03c75
Rabbani <mdgolamrabbani96@gmail.com>
2021-03-02 10:20:13 +0600

    pandas and html+css file added

--- WORK_ON_PDF/ReadPDF.py
+++ WORK_ON_PDF/ReadPDF.py
@@ -1,12 +1,12 @@
 import pyttsx3
 import PyPDF2
-book = open('MLBOOK.pdf', 'rb')
+book = open('sentimental.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print("Total pages = ",pages)
 
 speaker = pyttsx3.init()
-for num in range(5,pages):
+for num in range(4,pages):
     print("Reading page no:",num)
     page = pdfReader.getPage(num)
     text = page.extractText()
